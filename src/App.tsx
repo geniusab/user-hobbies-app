f5b441c9d09cca8f9cc6e5a6f8a83ba5bcff5b24
Anton Budagov <a.budagov@artk.ai>
2019-09-04 13:59:00 +0300

    !fix: pre-commit;

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
-import React, { FunctionComponent, useState } from 'react';
-import { connect } from 'react-redux';
+import React, { FunctionComponent, useState } from 'react'
+import { connect } from 'react-redux'
 import Header from './components/Header'
-import List from "./components/List";
-import './App.scss';
+import List from './components/List'
+import './App.scss'
 
-import Forms from "./components/Form";
-import mock_users from "./service/mock";
-import User from "./interfaces/User.interface";
-import { addUser } from "./redux/actions";
-import AddUser from "./components/Form/AddUser";
+import Forms from './components/Form'
+import mock_users from './service/mock'
+import User from './interfaces/User.interface'
+import { addUser } from './redux/actions'
+import AddUser from './components/Form/AddUser'
 
 type AppProps = {
-    users: Array<User>
-    addUser: Function
+  users: Array<User>
+  addUser: Function
 }
 
-
-const App: FunctionComponent<AppProps> = (props) => {
-    const {users, addUser} = props;
-    return (
-        <div className="App">
-            <List users={users}/>
-            <AddUser addUser={addUser}/>
-        </div>
-    );
+const App: FunctionComponent<AppProps> = props => {
+  const { users, addUser } = props
+  return (
+    <div className="App">
+      <List users={users} />
+      <AddUser addUser={addUser} />
+    </div>
+  )
 }
 
 const mapStateToProps = (state: any) => ({
-    users: state.users
-});
+  users: state.users,
+})
 
-export default connect(mapStateToProps, {addUser})(App);
+export default connect(
+  mapStateToProps,
+  { addUser },
+)(App)
